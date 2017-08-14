5926ab11b41f5fea482789d3b5770c33d502890d
Mackey22 <jjm447@cornell.edu>
2017-08-14 11:26:58 -0700

    Correct formatting based on return type. native split -> shlex split

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -1,8 +1,11 @@
 import json
 import logging
+import shlex
 from urllib.parse import parse_qs
 
 import hca
+import requests
+
 from chalice import Chalice, Response
 
 app = Chalice(app_name='slack_bot')
@@ -11,21 +14,35 @@ app.log.setLevel(logging.DEBUG)
 
 @app.route('/hca', methods=['GET', 'POST'], content_types=["application/x-www-form-urlencoded"])
 def index():
-    app.log.debug("IN index")
+    app.log.debug("In index")
 
     body = parse_qs(app.current_request.raw_body.decode())
     app.log.debug(body)
 
     # Every parameter comes wrapped in an array for some reason
-    text = body['text'][0]
-    args = text.split()
+    command_args_text = body['text'][0]
+    args = shlex.split(command_args_text)
     cli = hca.cli.CLI()
-    response = cli.make_request(args)
 
-    formatted_json = json.dumps(response.json(), indent=4)
+    payload_text = ""
+
+    try:
+        response = cli.make_request(args)
+
+        if isinstance(response, requests.Response):
+            payload_text = json.dumps(response.json(), indent=4)
+        elif isinstance(response, str):
+            payload_text = response
+        else:
+            payload_text = json.dumps(response, indent=4)
+
+    except Exception as e:
+        app.log.debug(e)
+        payload_text = e.args[0]
 
     payload = {'response_type': "in_channel",
-               'text': formatted_json}
+               'text': payload_text}
+
     return Response(body=payload,
                     status_code=200,
                     headers={'Content-Type': 'text/plain'})
