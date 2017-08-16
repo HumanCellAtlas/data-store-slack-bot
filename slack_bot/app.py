ecc89edb9f6a88a40b4b0c950348ef4e549cf41b
Mackey22 <jjm447@cornell.edu>
2017-08-16 14:26:28 -0700

    Print to channel

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -3,7 +3,7 @@ import logging
 import shlex
 from urllib.parse import parse_qs
 
-import hca
+import hca.cli
 import requests
 
 from chalice import Chalice, Response
@@ -12,7 +12,7 @@ app = Chalice(app_name='slack_bot')
 app.log.setLevel(logging.DEBUG)
 
 
-@app.route('/hca', methods=['GET', 'POST'], content_types=["application/x-www-form-urlencoded"])
+@app.route('/hca', methods=['POST'], content_types=["application/x-www-form-urlencoded"])
 def index():
     app.log.debug("In index")
 
@@ -22,33 +22,33 @@ def index():
     # Every parameter comes wrapped in an array for some reason
     command_args_text = body['text'][0]
     args = shlex.split(command_args_text)
-    cli = hca.cli.CLI()
+    app.log.debug(args)
 
+    cli = hca.cli.CLI()
     payload_text = ""
 
     try:
         response = cli.make_request(args)
 
         if isinstance(response, requests.Response):
-            if response.headers['content-length'] > 2 ** 20:
+            if 'Content-Length' in response.headers and int(response.headers['Content-Length']) > 2 ** 20:
                 payload_text = "Content too long to show in slack. Try using the cli or python bindings."
             else:
                 payload_text = response.content.decode()
-                app.log.debug("Response content decode type is {}".format(type(payload_text)))
         elif isinstance(response, str):
             payload_text = response
         else:  # response type is json
             payload_text = json.dumps(response, indent=4)
     except UnicodeDecodeError as e:
-        app.log.debug(e)
-        payload_text = "Unicode could not be decoded. The file is likely gzipped."
+        payload_text = "Content is not utf-8 encodable"
     except Exception as e:
-        app.log.debug(e)
-        payload_text = e.args[0]
+        app.log.debug("Exception")
+        payload_text = str(e)
 
+    app.log.debug(payload_text)
     payload = {'response_type': "in_channel",
                'text': payload_text}
 
     return Response(body=payload,
                     status_code=200,
-                    headers={'Content-Type': 'text/plain'})
+                    headers={'Content-Type': 'application/json'})
